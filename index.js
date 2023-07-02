fc486e53240ba8c0d47e3b01c9a6c24b74233db2
Shane Phillips <shane@loongphillips.com>
2023-07-02 11:49:59 -0400

    localStorage.getItem()

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
 let count = 0;
+let random = 0;
+
+if (localStorage.getItem(0)) {
+	saveEl.textContent +=
+		" " + localStorage.getItem(0);
+}
 
 function increment() {
 	count++;
@@ -11,7 +17,11 @@ function increment() {
 const save = () => {
 	let countStr = " " + count + " -";
 	saveEl.textContent += countStr;
-	localStorage.setItem("count", count);
+	if (random === 5) {
+		random = 0;
+	}
+	localStorage.setItem(random, count);
+	random += 1;
 	countEl.textContent = 0;
 	count = 0;
 	// console.log(count);
